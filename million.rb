3dfd1ab8047b5cea3196d3b4b90e6952271effd7
Steve Smith <stevesmith2609@gmail.com>
2017-01-19 14:48:37 +0000

    Refactor and add inline comments

--- million.rb
+++ million.rb
@@ -1,4 +1,5 @@
-def get_unit_value(number)
+# Renamed attribute to "value" as this will sometimes be a Fixnum, and sometimes a string
+def get_unit_value(value)
   in_words = {0 => "",
               1 => "one",
               2 => "two",
@@ -9,10 +10,11 @@ def get_unit_value(number)
               7 => "seven",
               8 => "eight",
               9 => "nine"}
-  if number.class == Fixnum
-    return in_words[number]
-  elsif number.class == String
-    return in_words.key(number)
+  if value.class == Fixnum
+    return in_words[value]
+  # For the teens exception, I'll sometime send a string here to convert back to a number
+  elsif value.class == String
+    return in_words.key(value)
   end
 end
 
@@ -43,72 +45,102 @@ def get_teen_value(number)
   return in_words[number]
 end
 
-def stringify(number)
+def find_length(number)
   strung = number.to_s
+  length = strung.length
 end
 
-def find_length(number)
-  strung = stringify(number)
-  length = strung.length 
+def get_units_words(position, character)
+  if position > 1
+    # If we're in a thousand's position, add the word "thousand"
+    if position % 6 == 4
+      @word << "thousand"
+    # If we're in the million's position, add the word "million"
+    elsif position % 6 == 1
+      # If there are zero units of 1000 (i.e. one million)
+      if @word[-1] == "" && @word[-2] == "" && @word[-3] == ""
+        # we need to remove the word "thousand"
+        @word.pop(4)
+      end
+      @word << "million"
+    end
+  end
+  # Add the unit value of the character
+  @word << get_unit_value(character)
+end
+
+def get_tens_words(character)
+  # If we need to convert the previously collected unit value to a teen value
+  if character == 1
+    # Convert the previously gotten unit word back to a number
+    unit = get_unit_value(@word.pop)
+    teen = 10 + unit
+    @word << get_teen_value(teen)
+  else
+    @word << get_tens_value(character)
+  end
+end
+
+def get_hundreds_words(position, character)
+  # Unless it's a whole hundred, it needs the "and" word to describe the 10s and/or units.
+  unless @word[-1] == "" && @word[-2] == ""
+    @word << "and"
+  end
+  if character == 0
+    @word << ""
+  else
+    @word << "hundred"
+    @word << get_unit_value(character)
+  end
+end
+
+# Reverses the string back to correct order, removes any empty words and joins
+def clean_up
+  # Reverse string back to correct order
+  @word = @word.reverse
+  # Filter out any "empty" words
+  @word = @word.select { |word| word.length != 0 }
+  # print all words with a space between each
+  puts @word.join(" ")
 end
 
 def wordify(number)
-  word = []
+  @word = []
   length = find_length(number)
-  strung = stringify(number).reverse
-  (0...length).each do |letter|
-    this = strung[letter].to_i
-    if letter%3 == 0
-      if letter > 0
-        if letter%6 == 3
-          word << "thousand"
-        elsif letter%6 == 0
-          if word[-1] == "" && word[-2] == "" && word[-3] == ""
-            word.pop(4)
-          end
-          word << "million"
-        end
-      end
-      word << get_unit_value(this)
-    elsif letter%3 == 1
-      if this == 1
-        unit = get_unit_value(word.pop)
-        teen = 10 + unit
-        word << get_teen_value(teen)
-      else
-        word << get_tens_value(this)
-      end
-    elsif letter%3 == 2
-      unless word[-1] == "" && word[-2] == ""
-        word << "and"
-      end
-      if this == 0
-        word << ""
-      else
-        word << "hundred"
-        word << get_unit_value(this)
-      end
+  # Reverse the number as a string so we can work from the units end
+  strung = number.to_s.reverse
+  (1..length).each do |position|
+    character = strung[position - 1].to_i
+    # If position is in the units (incl. thousands, millions) place...
+    if position % 3 == 1
+      get_units_words(position, character)
+    # If position is in the tens position...
+    elsif position % 3 == 2
+      get_tens_words(character)
+    # If position is in the hundreds postion...
+    elsif position % 3 == 0
+      get_hundreds_words(position, character)
     end
   end
-  word = word.reverse
-  word.each do |i|
-    unless i == ""
-      print i, " "
-    end
-  end
-  print "\n"
+  clean_up
 end
 
 (1..1000000).each do |number|
-  print number, " = "
-  wordify(number)
+  print number, " = ", wordify(number)
 end
 
-# IF LENGTH = 1, result = number to words
-# IF LENGTH = 2, result = tens value followed by unit value
-# EXCEPTION REQUIRED FOR TEENS
-# IF LENGTH = 3, result = number to words followed by "hundred and"
-# IF LENGTH = 4, result = number to words, followed by "thousand,"
+# IF LENGTH = 1, result = number to @words
+# IF LENGTH = 2, result = tens value followed by re-use LENGTH = 1
+# EXCEPTION REQUIRED FOR TEENS - will already have the unit value, it will need to be destroyed and reformed
 
-# So, for teen, I will have 
-# for 113, I have already the three, so I will have to destroy and replace it
+# IF LENGTH = 3, result = number to @words followed by "hundred and" and re-use LEGNTH = 2
+# IF LENGTH = 4, result = number to @words, followed by "thousand" and re-use LENGTH = 3
+
+# IF LENGTH = 5, result = re-use LENGTH = 2 and LENGTH = 4
+# IF LENGTH = 6, result = re-use LENGTH = 3 and LENGTH = 4
+
+# IF LENGTH = 7, result = number to @words, followed by "million" and LENGTH = 6
+
+# Following these rules, I could actually cope with any positive integer, as long as you don't mind
+# re-using the million pattern over and over
+# e.g. 1,000,000,000,000 would be expressed as 1 million million.
